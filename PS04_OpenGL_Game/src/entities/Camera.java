2c87778df34b3fcb602cfc689a2ee712c48f78ca
amit <amitzviklerr@hotmail.com>
2015-05-16 16:18:22 +0300

    position.y movement now available. Keys for movement are the following: A, D, W, S, Q, R

--- PS04_OpenGL_Game/src/entities/Camera.java
+++ PS04_OpenGL_Game/src/entities/Camera.java
@@ -19,6 +19,12 @@ public class Camera {
 		if(Keyboard.isKeyDown(Keyboard.KEY_S)){
 			position.z += 0.02f;
 		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_Q)){
+			position.y -= 0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_E)){
+			position.y += 0.02f;
+		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
 			position.x += 0.02f;
 		}
